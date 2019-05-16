0c6794a187914656f792eaef22d481a4827dd560
pk23488 <947612133@qq.com>
2019-05-16 13:04:57 +0800

    "5.16源程序中包含三种段"

---
+++ 5.16_1/2.asm
@@ -0,0 +1,26 @@
+assume cs:code, ds:data, ss:stack
+data segment    ;数据段
+		dw  0123H, 0456H, 0789H, 0abcH,0defH, 0fedH, 0cbaH, 0987H
+data ends
+
+stack segment   ;栈段
+		dw  0, 0, 0, 0, 0, 0, 0, 0    ;16个字的内容
+stack ends
+
+code segment    ;代码段
+
+start:		mov ax,stack
+				mov ss,ax
+				mov sp,16   ;sp指向栈顶地址16
+				mov ax,data
+				mov ds,ax
+				push ds:[0]
+				push ds:[2]
+				pop ds:[2]
+				pop ds:[0]
+				
+				mov ax,4c00h
+				int 21h
+				
+code ends
+end start     ;意味着代码段从start这里开始，一直到end start结束
